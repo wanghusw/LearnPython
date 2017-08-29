a000538e2448f4f351ea34fae5a1cb1bf7ea9d4a
wanghusw <wanghusw@gmail.com>
2017-08-29 15:58:18 +0800

    2017-08-29-learn-python

---
+++ old driver.py
@@ -0,0 +1,3 @@
+name = int(input("请输入你的年龄"))
+if name>=18:print("可以进入成人网站www.sbsb88.com")
+else:print("出门左拐www.liaoxuefeng.com")
---
+++ string.py
@@ -0,0 +1,8 @@
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+print('n =',n,'\nf =',f,'\ns1 =',s1,'\ns2 =',s2,'\ns3 =',s3,'\ns4 =',s4)
